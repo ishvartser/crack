9bc69a8b9b1e7f86bcdca647c709cf7301906329
ishvartser <ishvarts@ucsc.edu>
2014-03-07 21:20:47 -0800

    Starting to add functions

--- SuperWordSearch.java
+++ SuperWordSearch.java
@@ -2,7 +2,27 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 
+// Defines all possible substrings according to transition matrix
+class WordEntry {
+	public String[] letters;
+	public int[] location;
+}
+
 public class SuperWordSearch {
+
+	/* Generate transition matrix */
+
+	public void generateTransitionMatrix(char firstChar, String[] puzzle) {
+
+		
+
+	}
+
+	// Build all possible strings
+
+
+	// Check to see if one of possible strings match with word in wordbank
+
 
 	public static void main (String[] args) {
 
@@ -12,20 +32,30 @@ public class SuperWordSearch {
  			File file = new File(args[0]);
  			BufferedReader reader = new BufferedReader (new FileReader(file));
  			String line = null;
- 			int[] values = new int[10];
- 			String[] words = new String[10];
+ 			int[] values = new int[10]; // this value needs to be changed
+ 			String[] words = new String[10]; // this value needs to be changed
  			int i = 0;
- 			int ctr = 0;
+ 			int w = 0;
+ 			int whereWordsCtr = 0;
  			String wrapInfo = "";
  			boolean wrap;
 
- 			// Clean up input & read WRAP or NO_WRAP option
+ 			// Clean up input, get words, & read WRAP or NO_WRAP option
  			while ((line = reader.readLine()) != null) {
  				line = line.trim();
- 				if (!line.equals("") && ctr > 5) {
-	 				System.out.println ("line: " + line);
-	 				ctr++;
-	 			}
+ 				if (!line.equals("")) {
+ 					// Get words from wordbank
+ 					if (whereWordsCtr > 5) { // (# of rows + 2)
+	 					System.out.println ("Word: " + line);
+	 					words[w] = line;
+	 					w++;
+	 				}
+	 				// Get puzzle from file
+	 				else if (whereWordsCtr > 0 && whereWordsCtr < 4) { // (>0, <#rows+1)
+	 					System.out.println ("Puzzle: " + line);
+	 				}
+	 				whereWordsCtr++;
+	 			
 	 			if (line.equals("WRAP")) {
 	 				wrap = true;
 	 				System.out.println ("WrapTrue: " + wrap);
@@ -33,6 +63,7 @@ public class SuperWordSearch {
 	 			if (line.equals("NO_WRAP")) {
 	 				wrap = false;
 	 				System.out.println ("WrapFalse: " + wrap);
+	 			}
 	 			}
  			}
 
