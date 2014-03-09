de326f64fbab837b33f306524605b7c256fc6051
ishvartser <ishvarts@ucsc.edu>
2014-03-08 16:35:23 -0800

    Adding transition matrix logic

--- SuperWordSearch.java
+++ SuperWordSearch.java
@@ -5,7 +5,7 @@ import java.lang.*;
 // Defines all possible substrings according to transition matrix
 class WordEntry {
 	public String[] letters;
-	public int[] location;
+	public int[][] location;
 }
 
 class PuzzleEntry {
@@ -15,6 +15,11 @@ class PuzzleEntry {
 
 class SuperWordSearch {
 
+
+	/* Retrieve location of letter in */
+
+
+
 	/* Generate transition matrix */
 
 	// need to pass in location here, not the actual first character
@@ -23,21 +28,61 @@ class SuperWordSearch {
 		char[] possibleTransitionLetter = new char[100]; // Value needs to be changed
 		int[] letterLocation = new int[100]; // Value needs to be changed
 		int ctr = 0;
+		int[][] directions = new int[][] { {-1,-1},
+												   {-1,0},
+												   {-1,1},
+												   {0,1},
+												   {1,1},
+												   {1,0},
+												   {1,-1},
+												   {0,-1} };
 		// if some letter is 1 away from firstChar, add it to possibleTransitionLetter[]
 		for (int i=0; i<puzzle.length; i++) {
 			for (int j=0; j<puzzle[i].length; j++) {
 				//System.out.println ("firstChar: "+ firstChar);
 				//System.out.println ("generate..Puzzle: "+ puzzle[i][j]);
-				if (letterLocation[i]-1 == i ||
-						letterLocation[i]+1 == i ||
-						letterLocation[j]-1 == j ||
-						letterLocation[j]+1 == j) {
+				System.out.println ("firstChar: "+firstChar);
+				System.out.println ("puzzle[i][j]: "+puzzle[i][j]);
+				if (firstChar == puzzle[i][j]) {
+					// get location of char, which are the current indices
+					letterLocation[0] = i;
+					letterLocation[1] = j;
+					System.out.println ("[i]: "+i);
+					System.out.println ("[j]: "+j);
 
-					possibleTransitionLetter[ctr] = puzzle[i][j];
-					System.out.println ("possibleTransitionLetter at "+ctr+" : "+possibleTransitionLetter[ctr]);
-					ctr++;
+					List<Integer> res = new ArrayList<Integer>();
+				    for (int[] direction : directions) {
+				        int cx = i + direction[0];
+				        System.out.println ("direction[0]: "+direction[0]);
+				        System.out.println ("cx: "+cx);
+				        int cy = j + direction[1];
+				        System.out.println ("direction[1]: "+direction[1]);
+				        System.out.println ("cy: "+cy);
+				        if(cy >=0 && cy < puzzle.length) {
+				            if(cx >= 0 && cx < puzzle[cy].length){
+				                //res.add(puzzle[cy][cx]);
+				                System.out.println("Added surrounding letter... "+puzzle[cx][cy]);
+				            }
+				        }
+				    }
+
+					// now loop through the puzzle to find possible transitions
+					// for (int a=0; a<puzzle.length; a++) {
+					// 	for (int b=0; b<puzzle[a].length; b++) {
+						
+					// 		// if current index 1 away from letterLocation, then possible transition found
+					// 		if (letterLocation[0]-1 == a ||
+					// 			letterLocation[0]+1 == a ||
+					// 			letterLocation[1]-1 == b ||
+					// 			letterLocation[1]+1 == b) {
+
+					// 			possibleTransitionLetter[ctr] = puzzle[i][j];
+					// 			System.out.println ("possibleTransitionLetter at "+ctr+" : "+possibleTransitionLetter[ctr]);
+					// 			ctr++;
+					// 		}
+					// 	}
+					// }
 				}
-
 			}
 		}
 
@@ -68,6 +113,10 @@ class SuperWordSearch {
 
 		int numCols;
 
+		WordEntry wordEntry = new WordEntry();
+		wordEntry.letters = new String[12];
+		wordEntry.location = new int[5][5];
+
 		// Clean up input, get words, & read WRAP or NO_WRAP option
 		while ((line = reader.readLine()) != null) {
 			line = line.trim();
@@ -86,6 +135,7 @@ class SuperWordSearch {
 					System.out.println ("Letters added...");
 					for (int a=0; a<puzzle.length; a++) {
 						puzzle[whereWordsCtr-1][a] = line.charAt(a);
+
 						System.out.print(" " + puzzle[whereWordsCtr-1][a] + " ");
 						numCols++;
 					}
@@ -104,7 +154,7 @@ class SuperWordSearch {
 			}
 		}
 
-		System.out.println ("puzzle: "+ puzzle[0][0]);
+		System.out.println ("First word's 1st letter: "+ words[0].charAt(0));
 		generateTransitionMatrix(words[0].charAt(0), puzzle);
 
 		// Extract matrix dimension & number of words (last entry)
